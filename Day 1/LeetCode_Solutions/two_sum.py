49085d57d9a3fb8cb297a984c46286d793a2cade
parth tiwari <ntiwari1977nt17@gmail.com>
2025-04-30 02:26:08 +0530

    Empty commit for project milestone

--- Day 1/LeetCode_Solutions/two_sum.py
+++ Day 1/LeetCode_Solutions/two_sum.py
@@ -1,14 +1,14 @@
-def twosum(nums, target):
-    seen = {}
-
-    for i, num in enumerate(nums):
-        complement = target - num
-
-    if complement in seen:
-        return [seen[complement], i]
-    seen[num] = i
-    return []
-
-
-nums = list[input()]
-target = int(input())
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}  # Dictionary to store numbers we've already seen
+        for i, num in enumerate(nums):  # Loop through the list with both index and value
+            complement = target - num  # Calculate the complement (what we need to reach target)
+            
+            # Check if the complement already exists in the dictionary
+            if complement in seen:
+                return [seen[complement], i]  # Return indices of the complement and current number
+            
+            # If not found, add the current number and its index to the dictionary
+            seen[num] = i
+        
+        return []  # If no solution is found
